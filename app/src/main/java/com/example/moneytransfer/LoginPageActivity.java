21a805cac6278a91b8a205b6d1204836a7647ffb
unknown <jlee3688@gatech.edu>
2022-04-10 17:13:04 -0400

    add login page xml

--- app/src/main/java/com/example/moneytransfer/LoginPageActivity.java
+++ app/src/main/java/com/example/moneytransfer/LoginPageActivity.java
@@ -1,22 +1,18 @@
 package com.example.moneytransfer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FirstPageActivity extends AppCompatActivity {
+public class LoginPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firstpage);
+        setContentView(R.layout.activity_loginpage);
     }
 
     public void userClickSignIn(View view) {
-        Intent intent = new Intent(getApplicationContext(), LoginPageActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
