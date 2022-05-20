fb5cec47e3e9d5861d61689c41c15ce634b2be0d
Taehee <tpark94@gatech.edu>
2022-05-20 15:51:17 -0500

    Fix backbutton bug at main

--- TechPay/component/Main.js
+++ TechPay/component/Main.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { AppBar } from "@react-native-material/core";
+import { AppBar, IconButton } from '@react-native-material/core';
+import { StyleSheet, View, Text, BackHandler, Alert } from 'react-native';
+import { Icon } from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 const Main = () => {
+    const user = auth().currentUser;
+
+    useFocusEffect(
+            React.useCallback(() => {
+                const onBackPress = () => {
+                    Alert.alert("Exit", "Do you want to exit the application?",
+                        [
+                            {
+                                text: "Cancel",
+                                onPress: () => null,
+                                style: "cancel"
+                            },
+                            {
+                                text: "Yes",
+                                onPress: () => BackHandler.exitApp()
+                            }
+                        ]);
+                    return true;
+                };
+
+                BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+                return () =>
+                    BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+                }, []),
+            );
+
     return (
         <>
             <AppBar titleStyle = {styles.titles}
                 title = "My Account"
                 color = "#F9F9F9"
                 tintColor = "black"
-                centerTitle = "True" />
+                centerTitle = "True"
+                />
+            <View>
+                <Text> Welcome {user?.displayName} </Text>
+            </View>
+
         </>
     );
 };
