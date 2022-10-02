3835f921c73b4cdd756a5372535cfbda4cd48f49
Ha Jin Kim <hajinkim@lawn-128-61-3-145.lawn.gatech.edu>
2022-10-01 23:11:44 -0400

    progress 5:mainpage

---
+++ Test2/app/src/main/java/com/example/test2/PayeeInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayeeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payee_info);
+    }
+}
