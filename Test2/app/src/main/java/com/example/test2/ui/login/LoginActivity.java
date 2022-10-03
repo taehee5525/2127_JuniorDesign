0532b267845c61f98f821d8d96ac78e68cd492af
hyunhee804 <hyunheep1202@gmail.com>
2022-10-02 22:11:54 -0400

    edit sign in button

--- Test2/app/src/main/java/com/example/test2/ui/login/LoginActivity.java
+++ Test2/app/src/main/java/com/example/test2/ui/login/LoginActivity.java
@@ -59,7 +59,6 @@ public class LoginActivity extends AppCompatActivity {
                 if (loginFormState == null) {
                     return;
                 }
-                loginButton.setEnabled(loginFormState.isDataValid());
                 if (loginFormState.getUsernameError() != null) {
                     usernameEditText.setError(getString(loginFormState.getUsernameError()));
                 }
@@ -120,15 +119,6 @@ public class LoginActivity extends AppCompatActivity {
                             passwordEditText.getText().toString());
                 }
                 return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
             }
         });
 
