830438f62f85ff0c29abecf17060b8af71e82391
Jeonghoon Lee <jeonghoonlee@Jeonghoonui-MacBookPro.local>
2022-10-08 02:40:15 -0400

    Add Backend skeleton Spring Project

---
+++ backendserver/src/test/java/com/moneytransfer/backendserver/BackendserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneytransfer.backendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
