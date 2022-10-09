52296b3d1d9b395d74dadc4e3ab304182d06eb4e
Chaewon382 <chaewon.park382@gmail.com>
2022-10-09 03:46:51 -0400

    Add javadocs for byteToArrToString

--- backendserver/src/main/java/com/moneytransfer/backendserver/Util.java
+++ backendserver/src/main/java/com/moneytransfer/backendserver/Util.java
@@ -1,6 +1,11 @@
 package com.moneytransfer.backendserver;
 
 public class Util {
+    /**
+     * Converts an array of bytes to a String.
+     * @param temp byte array to be converted
+     * @return String coverted from byte array
+     */
     public static String byteArrToString(byte[] temp) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < temp.length; i++) {
