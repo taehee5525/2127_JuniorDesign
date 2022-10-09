797efe323efa9bbc407b602ad9aaae1f1d818b15
Ha Jin Kim <hajinkim@lawn-128-61-12-16.lawn.gatech.edu>
2022-10-09 14:34:08 -0400

    add transaction fail layout

---
+++ Test2/app/src/main/java/com/example/test2/TransactionFail.java
@@ -0,0 +1,14 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionFail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_fail);
+    }
+}
