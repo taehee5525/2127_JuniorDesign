7390a7c518907015186a44c2dabc33afb9b4e631
unknown <jlee3688@gatech.edu>
2022-10-09 21:49:02 -0400

    Add ErrorDecoder code to interact with testercode. The root cause of failure cannot be determined, but it can be solved with this code.

--- backendserver/src/main/java/com/moneytransfer/backendserver/Util.java
+++ backendserver/src/main/java/com/moneytransfer/backendserver/Util.java
@@ -14,7 +14,19 @@ public class Util {
         return sb.toString();
     }
 
-    public static String errorDecoder(String input) {
-        return null;
+    /**
+     * Encoding the String from unknown response formatting
+     * (we will use this until we figure out what cause the error).
+     * The error occurs when a tester code requests a query to this server.
+     * It has a string with an unknown format. But it can be solved by
+     * this method.
+     * We will only use this method while using the tester code.
+     * @param input String req that we got from the tester
+     * @return encoded String.
+     * @throws UnsupportedEncodingException
+     */
+    public static String errorDecoder(String input) throws UnsupportedEncodingException {
+        String temp = URLDecoder.decode(input, "ASCII").replaceAll("/", "");
+        return temp.substring(0, temp.length() - 1);
     }
 }
