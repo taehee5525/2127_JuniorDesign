eb026ccfe32f5bbb8aed138ba1264bf17f46aa44
Rami <85512110+ramib20@users.noreply.github.com>
2022-10-24 00:42:26 -0400

    connected TransactionSuccess page
    also fixed and centered an overlapping button

--- Test2/app/src/main/java/com/example/test2/PayeeInfoActivity.java
+++ Test2/app/src/main/java/com/example/test2/PayeeInfoActivity.java
@@ -2,12 +2,29 @@ package com.example.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class PayeeInfoActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payee_info);
+
+        button = (Button) findViewById(R.id.completeTransaction);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openTransactionSuccess(); }
+        });
+    }
+
+    public void openTransactionSuccess() {
+        Intent intent = new Intent(this, TransactionSuccess.class);
+        startActivity(intent);
     }
 }
--- Test2/app/src/main/java/com/example/test2/TransactionSuccess.java
+++ Test2/app/src/main/java/com/example/test2/TransactionSuccess.java
@@ -3,12 +3,36 @@ package com.example.test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import com.example.test2.ui.login.MainActivity;
+
+
 
 public class TransactionSuccess extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_success);
+
+        button = (Button) findViewById(R.id.completeTransaction);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToMain();
+            }
+        });
+
     }
+
+    private void returnToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
