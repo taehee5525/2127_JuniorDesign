142c169deea67c5a675317b1260e979f142411d4
hyunhee804 <hyunheep1202@gmail.com>
2022-11-04 00:51:34 -0400

    organize ui.login and added restriction features on Email and Password

--- Test2/app/src/main/java/com/example/test2/ui/login/LoginActivity.java
+++ Test2/app/src/main/java/com/example/test2/ui/login/LoginActivity.java
@@ -1,102 +1,71 @@
 package com.example.test2.ui.login;
+import com.example.test2.R;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
 
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.test2.CreateAccActivity;
 import com.example.test2.InstructionsPage;
 import com.example.test2.LoginFail;
 import com.example.test2.LoginSuccess;
-import com.example.test2.R;
-import com.example.test2.ui.login.LoginViewModel;
-import com.example.test2.ui.login.LoginViewModelFactory;
-import com.example.test2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
-    private ActivityLoginBinding binding;
-    private Button signupBtn;
     private ImageButton imgButton;
-    private Button loginBtn;
+    private Button signupBtn, loginBtn;
+    private EditText username, password;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        checkDangerousPermissions();
 
-        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
+        imgButton = (ImageButton) findViewById(R.id.helpBtn);
+        signupBtn = (Button) findViewById(R.id.btnSignUp);
+        loginBtn = (Button) findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
 
-        final EditText usernameEditText = binding.username;
-        final EditText passwordEditText = binding.password;
-        final ProgressBar loadingProgressBar = binding.loading;
-
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+        // Help Page
+        imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
+            public void onClick(View view) {
+                openHelpPage();
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+        // Sign Up Page
+        signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                    //switch screen
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
+            public void onClick(View view) {
+                openSignup();
             }
         });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
+                if (password.getText().toString().length() < 5 ||
+                        password.getText().toString() == null) {
+                    password.setError("password minimum contain 5 character");
+                }
             }
 
             @Override
@@ -106,60 +75,46 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                //String email = username.getText().toString().trim();
+
+                if (username.getText().toString() == null ||
+                        !Patterns.EMAIL_ADDRESS.matcher(username.getText().toString()).matches()) {
+                    username.setError("email address form");
+
+                } else if (password.getText().toString().length() < 5 ||
+                        password.getText().toString() == null) {
+                    password.setError("password minimum contain 5 character");
+
+                }
             }
         };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        username.addTextChangedListener(afterTextChangedListener);
+        password.addTextChangedListener(afterTextChangedListener);
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
 
-        signupBtn = (Button) findViewById(R.id.btnSignUp);
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSignup();
-            }
-        });
-
-        imgButton = (ImageButton) findViewById(R.id.helpBtn);
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openHelpPage();
-            }
-        });
-
-        // Backend part: check database & proceed to either success page or failed page
-        loginBtn = (Button) findViewById(R.id.login);
+        // Login Page
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (true) {
+                if (username.getText().toString().length() != 0 &&
+                    password.getText().toString().length() != 0) {
+                    String id = username.getText().toString();
+                    String pwd = password.getText().toString();
+
                     openLoginSuccess();
-                } else {
+                }else {
                     openLoginFail();
                 }
             }
         });
     }
-    private void openSignup() {
-        Intent intent = new Intent(this, CreateAccActivity.class);
+    private void openHelpPage() {
+        Intent intent = new Intent(this, InstructionsPage.class);
         startActivity(intent);
     }
 
-    private void openHelpPage() {
-        Intent intent = new Intent(this, InstructionsPage.class);
+    private void openSignup() {
+        Intent intent = new Intent(this, CreateAccActivity.class);
         startActivity(intent);
     }
 
@@ -173,14 +128,32 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "authorized", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "no authorized", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "need help with authorization.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
     }
 
 }
