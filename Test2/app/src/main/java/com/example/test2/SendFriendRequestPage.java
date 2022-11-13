7e8e9d10fe957863b0825ccaca42e3bd4f15e1c8
hyunhee804 <hyunheep1202@gmail.com>
2022-11-13 18:59:25 -0500

    add AddFriend page

--- Test2/app/src/main/java/com/example/test2/SendFriendRequestPage.java
+++ Test2/app/src/main/java/com/example/test2/SendFriendRequestPage.java
@@ -2,12 +2,16 @@ package com.example.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.test2.ui.login.LoginActivity;
+import com.example.test2.ui.login.MainActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,7 +20,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SendFriendRequestPage extends AppCompatActivity {
-    private Button sendReq;
     private EditText email;
 
     private Map<String, String> headerMap = new HashMap<>();
@@ -30,6 +33,16 @@ public class SendFriendRequestPage extends AppCompatActivity {
 
         email = (EditText) findViewById(R.id.username);
 
+        Button sendReq, backToMain;
+
+        backToMain = (Button) findViewById(R.id.gotoMainPageBtn);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain();
+            }
+        });
+
 
         sendReq = (Button) findViewById(R.id.addFriendBtn);
         sendReq.setOnClickListener(new View.OnClickListener() {
@@ -39,12 +52,33 @@ public class SendFriendRequestPage extends AppCompatActivity {
                     CustomTask task = new CustomTask();
                     String result = task.execute(email.getText().toString()).get();
                     Log.w("add friend", result);
+
+                    if (result.contains("t")) {
+                        openSendReqSuccPage();
+                    } else {
+                        openSendReqFailPage();
+                    }
                 } catch (Exception ignored) {
 
                 }
 
             }
         });
+    }
+
+    private void openMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSendReqSuccPage() {
+        Intent intent = new Intent(this, SendRequestSucc.class);
+        startActivity(intent);
+    }
+
+    private void openSendReqFailPage() {
+        Intent intent = new Intent(this, SendRequestFail.class);
+        startActivity(intent);
     }
 
     class CustomTask extends AsyncTask<String, Void, String> {
