a8f3ff358748978c475f68ba9efb54c70244cdd7
Ha Jin Kim <hajinkim@lawn-128-61-12-137.lawn.gatech.edu>
2022-12-03 14:21:30 -0500

    add external user activity xml page

---
+++ Test2/app/src/main/java/com/example/test2/ExternalUser.java
@@ -0,0 +1,14 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExternalUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external_user);
+    }
+}
