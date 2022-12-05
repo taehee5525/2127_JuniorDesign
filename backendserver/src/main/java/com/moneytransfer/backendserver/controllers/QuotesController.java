b85f5b356336c04a22bec89aee764741783eca35
Taehee Park <taehee5525@gmail.com>
2022-12-05 05:09:07 -0500

    Added condition

--- backendserver/src/main/java/com/moneytransfer/backendserver/controllers/QuotesController.java
+++ backendserver/src/main/java/com/moneytransfer/backendserver/controllers/QuotesController.java
@@ -2,12 +2,14 @@ package com.moneytransfer.backendserver.controllers;
 
 import com.moneytransfer.backendserver.BackendserverApplication;
 import com.moneytransfer.backendserver.services.userService.QuotesService;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,7 +28,12 @@ public class QuotesController {
 
     @PutMapping(value = "quotes/{quotesId}")
     public void putQuotes(@RequestBody String body, @PathVariable UUID quotesID) {
-        
+
+    }
+
+    @PostMapping(value = "quotes")
+    public void postQuotes(@RequestBody String body) {
+        JSONObject data = new JSONObject(body);
     }
 
 }
